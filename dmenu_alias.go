a7779c54f4b6983e6a6a9ea093ff00fb8760111d
JL <jeremyli.gitemail@gmail.com>
2024-01-09 20:10:32 -0600

    updated names to following naming convention

--- dmenu_alias.go
+++ dmenu_alias.go
@@ -12,7 +12,7 @@ import (
 // Takes a line of the form str1=str2 and returns (str1, str2)
 // if the line is not properly formatted, return an error
 // Example: vi=nvim will return (vi, nvim)
-func parse_line(line string) (string, string, error) {
+func parseLine(line string) (string, string, error) {
 	alias := strings.Split(line, "=")
 
 	if len(alias) == 2 {
@@ -25,14 +25,14 @@ func parse_line(line string) (string, string, error) {
 }
 
 // Parse the aliast list and return a map
-func parse_alias_list(path string) map[string]string {
-	alias_list := make(map[string]string)
+func parseAliasList(path string) map[string]string {
+	aliasList := make(map[string]string)
 
 	//Attempt to open the file
 	file, err := os.Open(path)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error: file not found: %s\n", path)
-		return alias_list
+		return aliasList
 	}
 	defer file.Close()
 
@@ -40,19 +40,19 @@ func parse_alias_list(path string) map[string]string {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
-		s1, s2, err := parse_line(line)
+		s1, s2, err := parseLine(line)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, err.Error())
 			continue
 		}
 
-		alias_list[s1] = s2
+		aliasList[s1] = s2
 	}
 
-	return alias_list
+	return aliasList
 }
 
-func read_dmenu_output(alias_list map[string] string) {
+func readDmenuOutput(aliasList map[string] string) {
 	//Read stdin line by line and return the aliased string from the input
 	reader := bufio.NewReader(os.Stdin)
 
@@ -71,7 +71,7 @@ func read_dmenu_output(alias_list map[string] string) {
 
 		//Strip out the newlines from the line so we get the proper output
 		line = strings.Replace(line, "\n", "", -1)
-		str, ok := alias_list[line]
+		str, ok := aliasList[line]
 		if ok {
 			fmt.Println(str)
 		} else {
@@ -80,13 +80,13 @@ func read_dmenu_output(alias_list map[string] string) {
 	}
 }
 
-func output_dmenu_input(alias_list map[string] string) {	
-	dmenu_input := make([]string, 0)
+func outputDmenuInput(aliasList map[string] string) {	
+	dmenuInput := make([]string, 0)
 	set := make(map[string] bool)
 
 	//Insert alias strings into the set and input
-	for str := range alias_list {
-		dmenu_input = append(dmenu_input, str)
+	for str := range aliasList {
+		dmenuInput = append(dmenuInput, str)
 		set[str] = true
 	}
 	
@@ -110,24 +110,24 @@ func output_dmenu_input(alias_list map[string] string) {
 		_, exists := set[line]
 		//Make sure we don't double insert an element
 		if !exists {
-			dmenu_input = append(dmenu_input, line)
+			dmenuInput = append(dmenuInput, line)
 		}
 	}
 
-	sort.Strings(dmenu_input)
+	sort.Strings(dmenuInput)
 
 	//Output input to stdout to be piped into dmenu
-	for _, str := range dmenu_input {
+	for _, str := range dmenuInput {
 		fmt.Println(str)
 	}
 }
 
 const (
-	output_mode int = iota
-	input_mode
+	outputMode int = iota
+	inputMode
 )
 
-func get_path_from_args() string {
+func getPathFromArgs() string {
 	for i, arg := range os.Args {
 		//Ignore argument 0 (the executable name)
 		if i == 0 {
@@ -145,27 +145,27 @@ func get_path_from_args() string {
 	return os.Getenv("HOME") + "/.config/dmenu_alias_list"
 }
 
-func read_options() int {
+func readOptions() int {
 	for _, arg := range os.Args {
 		if arg == "-o" {
 			//-o means read output from dmenu
-			return output_mode
+			return outputMode
 		} else if arg == "-i" {
 			//-i means generate input for dmenu
-			return input_mode
+			return inputMode
 		}
 	}
-	return output_mode
+	return outputMode
 }
 
 func main() {
-	alias_list := parse_alias_list(get_path_from_args())	
-	switch read_options() {
-		case output_mode:
-			read_dmenu_output(alias_list)
+	aliasList := parseAliasList(getPathFromArgs())	
+	switch readOptions() {
+		case outputMode:
+			readDmenuOutput(aliasList)
 			break
-		case input_mode:
-			output_dmenu_input(alias_list)
+		case inputMode:
+			outputDmenuInput(aliasList)
 			break
 		default:
 			break
