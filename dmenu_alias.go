d5281e5a58c929c5fb717d620c8bd77bdf53993a
JL <jeremyli.gitemail@gmail.com>
2024-01-09 18:02:47 -0600

    provide input to dmenu

--- dmenu_alias.go
+++ dmenu_alias.go
@@ -6,11 +6,12 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"sort"
 )
 
-//Takes a line of the form str1=str2 and returns (str1, str2)
-//if the line is not properly formatted, return an error
-//Example: vi=nvim will return (vi, nvim)
+// Takes a line of the form str1=str2 and returns (str1, str2)
+// if the line is not properly formatted, return an error
+// Example: vi=nvim will return (vi, nvim)
 func parse_line(line string) (string, string, error) {
 	alias := strings.Split(line, "=")
 
@@ -23,9 +24,9 @@ func parse_line(line string) (string, string, error) {
 	return "", "", errors.New("Syntax error: " + line)
 }
 
-//Parse the aliast list and return a map
-func parse_alias_list(path string) map[string] string {
-	alias_list := make(map[string] string)
+// Parse the aliast list and return a map
+func parse_alias_list(path string) map[string]string {
+	alias_list := make(map[string]string)
 
 	//Attempt to open the file
 	file, err := os.Open(path)
@@ -45,33 +46,19 @@ func parse_alias_list(path string) map[string] string {
 			continue
 		}
 
-		alias_list[s1] = s2;
+		alias_list[s1] = s2
 	}
 
 	return alias_list
 }
 
-func main() {
-	alias_list_path := ""	
-	
-	if len(os.Args) == 1 {
-		//No arguments provided	
-		//Use the path $HOME/.config/dmenu_alias_list
-		//We assume $HOME is defined
-		home_path := os.Getenv("HOME") + "/.config/dmenu_alias_list"
-		alias_list_path = home_path
-	} else if len(os.Args) > 1 {
-		alias_list_path = os.Args[1]
-	}
-
-	alias_list := parse_alias_list(alias_list_path)
-
+func read_dmenu_output(alias_list map[string] string) {
 	//Read stdin line by line and return the aliased string from the input
 	reader := bufio.NewReader(os.Stdin)
 
 	for {
 		line, err := reader.ReadString('\n')
-	
+
 		//Empty line, end the program
 		if len(line) == 0 {
 			break
@@ -81,7 +68,7 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		
+
 		//Strip out the newlines from the line so we get the proper output
 		line = strings.Replace(line, "\n", "", -1)
 		str, ok := alias_list[line]
@@ -90,5 +77,97 @@ func main() {
 		} else {
 			fmt.Println(line)
 		}
+	}
+}
+
+func output_dmenu_input(alias_list map[string] string) {	
+	dmenu_input := make([]string, 0)
+	set := make(map[string] bool)
+
+	//Insert alias strings into the set and input
+	for str := range alias_list {
+		dmenu_input = append(dmenu_input, str)
+		set[str] = true
+	}
+	
+	//Read stdin line by line and return the aliased string from the input
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		line, err := reader.ReadString('\n')
+
+		//Empty line, end the program
+		if len(line) == 0 {
+			break
+		}
+
+		//Something went wrong with reading stdin, panic
+		if err != nil {
+			panic(err)
+		}
+
+		//Strip out the newlines from the line so we get the proper output
+		line = strings.Replace(line, "\n", "", -1)
+		_, exists := set[line]
+		//Make sure we don't double insert an element
+		if !exists {
+			dmenu_input = append(dmenu_input, line)
+		}
+	}
+
+	sort.Strings(dmenu_input)
+
+	//Output input to stdout to be piped into dmenu
+	for _, str := range dmenu_input {
+		fmt.Println(str)
+	}
+}
+
+const (
+	output_mode int = iota
+	input_mode
+)
+
+func get_path_from_args() string {
+	for i, arg := range os.Args {
+		//Ignore argument 0 (the executable name)
+		if i == 0 {
+			continue
+		}
+
+		if arg != "-o" && arg != "-i" {
+			return arg
+		}
+	}
+
+	//No arguments provided
+	//Use the path $HOME/.config/dmenu_alias_list
+	//We assume $HOME is defined
+	return os.Getenv("HOME") + "/.config/dmenu_alias_list"
+}
+
+func read_options() int {
+	for _, arg := range os.Args {
+		if arg == "-o" {
+			//-o means read output from dmenu
+			return output_mode
+		} else if arg == "-i" {
+			//-i means generate input for dmenu
+			return input_mode
+		}
+	}
+	return output_mode
+}
+
+func main() {
+	alias_list := parse_alias_list(get_path_from_args())	
+	switch read_options() {
+		case output_mode:
+			read_dmenu_output(alias_list)
+			break
+		case input_mode:
+			output_dmenu_input(alias_list)
+			break
+		default:
+			break
 	}
 }
